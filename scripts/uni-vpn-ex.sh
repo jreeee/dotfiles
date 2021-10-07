6165bd57665fd7e82b442774b7f748bf7d7453dd
jreeee <jeremias.michael.kilian@uni-weimar.de>
2021-10-07 12:20:11 +0200

    modified the vpn example script a bit

--- scripts/uni-vpn-ex.sh
+++ scripts/uni-vpn-ex.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-echo 'PASSWD' | sudo openconnect HOSTNAME --authgroup 'TUNNEL(GROUP)' -u USER --passwd-on-stdin --no-dtls
+if [ -z $1 ]; then
+	echo 'PASSWD' | sudo openconnect HOSTNAME --authgroup 'TUNNEL(GROUP)' -u USER --passwd-on-stdin &
+else
+	sudo pkill openconnect
+fi
 
 # this is not that smart since you expose your password, I'll likely update it someday...
 # for the time being if you use it (which you shouldn't tbf) at least make it read-write-
 # protected to somewhat protect your password, you'll have to be root to run it anyways
 
-# the --no-dtls option is for certain scenarios where the connection would otherwise not work
+# the --no-dtls option can be used for certain scenarios where the connection would otherwise not work
 # try to connect without it fist bc it slows down your connection a bit since it then only uses TCP
