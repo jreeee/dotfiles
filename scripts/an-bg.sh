d9fda9e588a6c47c278a433eb32abc395866e92d
jreeee <jeremias.michael.kilian@uni-weimar.de>
2021-12-23 01:23:23 +0100

    now working with exact resolutions

--- scripts/an-bg.sh
+++ scripts/an-bg.sh
@@ -7,17 +7,34 @@ PIDFILE="/var/run/user/$UID/bg.pid"
 
 declare -a PIDs
 
+_size() {
+  res=$(echo "$1" | grep -oP 'x\d+') # sth like 1920x1080-..., becomes x1080
+  res=${res:1} # 1080
+  num="$2"
+  num=${num: -1} # number of the video selected (like the themes)
+  type=$(ls "$2"| grep -oPm1 '\..*') # ending of the first file
+  file="$2"'/v'"$num"'-'"$res""$type" # full path
+  echo "$file"
+  if [ -f "$file" ]; then
+    _screen "$1" "$file"
+  else
+    #TODO read all files in the folder, calculate the one closest to the res
+    #play that file
+    exit 1
+  fi
+}
+
 _screen() {
-    xwinwrap -ov -ni -g "$1" -- mpv --fullscreen\
+  xwinwrap -ov -ni -g "$1" -- mpv --fullscreen\
         --no-stop-screensaver \
         --vo=gpu --hwdec=vaapi \
         --loop-file --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings \
         "$2" &
-	sleep 0.5
-	#the process tree has to be created first, hence the sleep
-    PIDs+=("$(pstree -p $! | grep -oP '\-mpv\(\d+'| grep -oP '\d+')")
-	# if we only kill xwinwrap mpv will not termiante but rather sleep,
-	# so instead we get mpv's id which also kills xwinwrap when killed
+  sleep 0.5
+  #the process tree has to be created first, hence the sleep
+  PIDs+=("$(pstree -p $! | grep -oP '\-mpv\(\d+'| grep -oP '\d+')")
+  # if we only kill xwinwrap mpv will not termiante but rather sleep,
+  # so instead we get mpv's id which also kills xwinwrap when killed
 }
 
 _modify() {
@@ -50,7 +67,11 @@ if [ $# -gt "0" ]; then
 
 	  for i in $( xrandr -q | grep ' connected' | grep -oP '\d+x\d+\+\d+\+\d+')
 	  do
-        _screen "$i" "$1"
+        if [ $# -gt "1" ]; then
+          _size "$i" "$1"
+        else
+          _screen "$i" "$1"
+        fi
 	  done
 
 	  printf "%s\n" "${PIDs[@]}" > $PIDFILE
