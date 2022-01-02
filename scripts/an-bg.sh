71d3d389bed1461497400032170a6864b5f7223d
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-01-02 14:00:13 +0100

    added some comments and fixed two bugs

--- scripts/an-bg.sh
+++ scripts/an-bg.sh
@@ -2,22 +2,24 @@
 
 # from https://github.com/CalinLeafshade/dots/blob/master/bin/bin/bg.sh
 # I've made some adjustments for my specific setup
-# for it to work all videos in one folder need to have the same file extension
+# except for the file-format you can name the files however you like and also
+# put other non-video files there
+# make sure that all videos in the folder have different resolutions
 
 PIDFILE="/var/run/user/$UID/bg.pid"
 
 declare -a PIDs
 declare -a VIDs
 declare -a VRHs # video resolution (only height)
-declare -r THREADS="8"
 
 _generate() {
+  threads=$(nproc --all)
   ffmpeg -i "$1" -vf scale=-1:"$2" -g 48 -keyint_min 48 \
-         -sc_threshold 0 -row-mt 1 -threads "$THREADS" -speed 2 -tile-columns 4 \
+         -sc_threshold 0 -row-mt 1 -threads "$threads" -speed 2 -tile-columns 4 \
          -loglevel 0 -stats "$3"
 }
 
-_size() {
+_scale() {
   VIDs=( "$2"* ) # add all the files in that directory to the array
   len=${#VIDs[@]}
 
@@ -28,7 +30,8 @@ _size() {
 
   # getting all the files that are videos and storing their height in VRHs
   type=''
-  for (( i=0;  i<"$len"; ++i )) do
+  for (( i=0; i<"$len"; ++i )) do
+    # getting the height of the video files
     res_i=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 "${VIDs[i]}" 2> /dev/null)
     if [ "$res_i" -eq "$res_i" ] 2> /dev/null; then
       VRHs+=("$res_i")
@@ -40,19 +43,21 @@ _size() {
     fi
   done
 
+  # making sure that there is at least one playable file in the directory
   if [ "$type" == '' ]; then
     echo 'ERROR: no files could be found, exiting'
     exit 1
   fi
 
+  # getting dimensions
   res=$(echo "$1" | grep -oP 'x\d+') # sth like 1920x1080-..., becomes x1080
   res=${res:1} # 1080
 
   # trying to find a matching resolution
   index="0"
   abs_diff="$res"
-  max_h_i="$res" # used for converting, assuming that the quality is the best due to the size
-  for (( i=0;  i<"$len"; ++i )) do
+  max_h_i="0" # used for converting, assuming that the quality is the best due to the resolution
+  for (( i=0; i<"$len"; ++i )) do
     res_i=${VRHs[i]}
     if [ "$res_i" -gt 0 ]; then
       dif=$(( res - res_i ))
@@ -62,6 +67,7 @@ _size() {
     fi
   done
 
+  # if the difference is zero we have a matching video and can play that
   if [ "$abs_diff" -eq "0" ]; then
     _screen "$1" "${VIDs[$index]}" &>/dev/null 2>&1
   else
@@ -77,6 +83,9 @@ _size() {
         else
           echo 'downscaling from '"$height"' to '"$res"
         fi
+
+        # a new file will be generated and saved in the video folder
+        # naming scheme: gen-<screen_height>.<video_format> i.e. gen-720.webm
         file="$2"'gen-'"$res""$type"
         _generate "$source" "$res" "$file"
         _screen "$1" "$file" &>/dev/null 2>&1
@@ -132,9 +141,10 @@ if [ $# -gt "0" ]; then
 
 	  for i in $( xrandr -q | grep ' connected' | grep -oP '\d+x\d+\+\d+\+\d+')
 	  do
+        # this would be the case if we used the script via setthm.sh
         if [ $# -gt "1" ]; then
-          _size "$i" "$1"
-        else
+          _scale "$i" "$1"
+        else # usual usecase: have a video played on all monitors
           _screen "$i" "$1"
         fi
 	  done
