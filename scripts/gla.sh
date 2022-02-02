7eeaa833b89e65570ecacd46fc4ab16e34c7829b
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-02-02 17:52:20 +0100

    update looks a bit nicer, removed a redundant line

--- scripts/gla.sh
+++ scripts/gla.sh
@@ -6,12 +6,11 @@ GIT="$HOME/git/"
 
 FOLDERS=( "$GIT"* )
 
-cd "$GIT"
-
 for (( i=1; i<${#FOLDERS[@]}; i++ )) do
-	cd "${FOLDERS[i]}"
+	cd "${FOLDERS[i]}" || exit 1
 	if [ -e ".git" ]; then
-		echo "updating ${FOLDERS[i]}"
+		name=$(echo "${FOLDERS[i]}" | rev | cut -d/ -f1 | rev)
+		echo "updating $name"
 		git pull
 	fi
 done
