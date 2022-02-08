e5d245985db98a0b8c3f5396b4072be5b5cfe37d
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-02-08 05:14:47 +0100

    added a patch/hook like option to the pull script

--- scripts/gla.sh
+++ scripts/gla.sh
@@ -9,7 +9,14 @@ FOLDERS=( "$GIT"* )
 for (( i=1; i<${#FOLDERS[@]}; i++ )) do
 	cd "${FOLDERS[i]}" || exit 1
 	if [ -e ".git" ]; then
+		tmp=$(find | grep -o *.patch)
+		if [ "$tmp" != "" ];then
+			eval "${FOLDERS[i]}/$tmp 1"
+		fi
 		echo "updating ${FOLDERS[i]:${#GIT}}"
 		git pull
+		if [ "$tmp" != "" ];then
+            eval "${FOLDERS[i]}/$tmp"
+        fi
 	fi
 done
