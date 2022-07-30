4d97294956dece9a9d29e3eabd674c5d28e8ca58
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-07-30 02:53:28 +0200

    better mensa script

--- scripts/mensa.sh
+++ scripts/mensa.sh
@@ -1,5 +1,12 @@
-# mensa script
+#! /bin/bash
+# mensa script for the vegetarian dishes
 
+#params
+id="151"
+col="\\\e[1;36m"
+clear="\\\e[0m"
+
+#hacky stuff
 [ $# -gt "0" ] && i=$(echo "$1 * 24" | bc -l) || i="10";
-str=$(curl -s 'https://openmensa.org/api/v2/canteens/151/days/'$(date -Ih -d "+$i hours")'/meals' | jq -j '.[] | select(.category|test("Veg*")) | .name, .prices.students' | sed -e "s/\([0-9].[0-79]\)\(\|$\\)/\10\2/g" -e "s/[0-9].[0-9]\{2\}/ &€/g" -e "s/€/€\n/g")
-[ "$str" == "" ] && echo "mensa is closed :(" || echo "$str"
+str=$(curl -s "https://openmensa.org/api/v2/canteens/$id/days/$(date -Ih -d "+$i hours")/meals" | jq -j '.[] | select(.category|test("Veg*")) | .name, .prices.students' | sed -e "s/\([0-9].[0-79]\)/\10/g;s/[0-9].[0-9]\{2\}/ $col&€$clear\n/g")
+[ "$str" == "" ] && echo "No Mensa?" || echo -e "Mensa Plan, $(date -d "+$i hours" "+%a%e %b %g"):\n$str"
