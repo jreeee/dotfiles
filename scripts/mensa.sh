87f50ad13f1e17cfb1e0cb911b119ca1da53014a
jreeee <jeremias.michael.kilian@uni-weimar.de>
2022-08-08 21:58:25 +0200

    added the option to select a mensa based on its id

--- scripts/mensa.sh
+++ scripts/mensa.sh
@@ -2,13 +2,13 @@
 # mensa script for the vegetarian dishes
 
 # params
-id="151"
 col1="\\\e[1;36m"
 col2="\\\e[1;33m"
 clear="\\\e[0m"
 
 # hacky stuff
-[ $# -gt "0" ] && i=$(echo "$1 * 24" | bc -l) || i="10";
+[ $# -gt "0" ] && i=$(echo "$1 * 24" | bc -l) || i="10" &&
+[ $# -gt "1" ] && id="$2" || id="151" &&
 str=$(curl -s "https://openmensa.org/api/v2/canteens/$id/days/$(date -Ih -d "+$i hours")/meals" | jq -j '.[] | select(.category|test("Veg*")) | .name, .prices.students' | sed -e "s/\([0-9]\.[0-9]\)\([^0-9]\|$\)/\10\2/g;s/[0-9]\.[0-9]\{2\}/ $col1&€$clear\n/g;s/^\|\(\\n\)\([^$]\)/\1$col2->$clear \2/g")
 [ "$str" == "" ] && echo "No Mensa?" || echo -e "$(curl -s "https://openmensa.org/api/v2/canteens/$id" | jq -j .name), $(date -d "+$i hours" "+%a%e %b %g"):\n$str"
 
