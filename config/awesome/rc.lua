c477efc45447fe17133aa659435475c073386c63
jreeee <jeremias.michael.kilian@uni-weimar.de>
2024-04-19 10:13:15 +0200

    enabled blur, disabled scrollwheel-based workspace switching

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -253,9 +253,9 @@ awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) 
 
 -- {{{ Mouse bindings
 root.buttons(my_table.join(
-    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end)
+    -- awful.button({ }, 4, awful.tag.viewnext),
+    -- awful.button({ }, 5, awful.tag.viewprev)
 ))
 -- }}}
 
